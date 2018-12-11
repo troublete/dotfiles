7d606984d94a2aad9eb72686cd36801a25d2a34a
Willi Eßer <willi.esser@troublete.com>
2018-12-11 11:50:13 +0100

    (make) made copy of .gitconfig static to avoid problems

--- copy-config.sh
+++ copy-config.sh
@@ -1,5 +1,5 @@
 # copy system configuration files to home directory
-for file in .aliases .gitconfig .gitignore .vimrc .zshrc
+for file in .aliases .gitignore .vimrc .zshrc
 do
 	ln -fs $PWD/conf/$file ~/$file
 done
